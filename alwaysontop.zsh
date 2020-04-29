0aa22adae039e6b0df56054149a3fb2cec92ae86
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-04-29 11:08:25 +0200

    Removed echos

--- alwaysontop.zsh
+++ alwaysontop.zsh
@@ -20,8 +20,6 @@ function alwaysontop {
         export ALWAYSONTOP="TRUE"
         add-zsh-hook precmd _gototop
     fi
-    
-    echo -e "[alwaysontop.sh] ${COLOR_BIPurple}always on top${COLOR_off} ${COLOR_BGreen}ON${COLOR_off}."
 }
 
 
@@ -31,8 +29,6 @@ function unalwaysontop {
         export ALWAYSONTOP="FALSE"
         add-zsh-hook -d precmd _gototop
     fi
-   
-    echo -e "[alwaysontop.sh] ${COLOR_BIPurple}always on top${COLOR_off} ${COLOR_BRed}OFF${COLOR_off}."
 }
 
 
@@ -49,15 +45,12 @@ function autoclear {
         }
         zle -N accept-line
     fi
-    
-    echo -e "[alwaysontop.sh] ${COLOR_BIYellow}autoclear${COLOR_off} ${COLOR_BGreen}ON${COLOR_off}."
 }
 
 
 function unautoclear {
     export AUTOCLEAR="FALSE"
     zle -A original-accept-line accept-line
-    echo -e "[alwaysontop.sh] ${COLOR_BIYellow}autoclear${COLOR_off} ${COLOR_BRed}OFF${COLOR_off}."
 }
 
 
@@ -91,16 +84,9 @@ function alwaysontop_help {
     echo -e "    "                 
     echo -e "    autoclear         Turn ${COLOR_BGreen}ON${COLOR_off} clear-screen after each command."
     echo -e "    unautoclear       Turn ${COLOR_BRed}OFF${COLOR_off} clear-screen after each command."
-    echo -e "    "
-    echo -e "    alwaysontop indicator:  ${COLOR_BIPurple}↑↑${COLOR_off}"
-    echo -e "    autoclear indicator:    ${COLOR_BIYellow}◎${COLOR_off}"
-
 }
 
 COLOR_off='\033[0m' 
-COLOR_BIPurple='\033[1;95m' 
-COLOR_BIYellow='\033[1;93m'
-COLOR_IBlack='\033[0;90m'
 COLOR_BGreen='\033[1;32m'
 COLOR_BRed='\033[1;31m'
 
